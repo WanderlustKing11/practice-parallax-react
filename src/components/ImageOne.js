8488546cd4f6b88b2664d50a42aef6c6ae0273da
Doug <wanderlustking11@gmail.com>
2023-06-27 13:45:59 -0400

    Add lodash scroll throttle (only to ImageOne)

--- src/components/ImageOne.js
+++ src/components/ImageOne.js
@@ -1,9 +1,24 @@
 import { Parallax } from 'react-parallax';
+import { useEffect } from 'react';
+import { throttle } from 'lodash';
 // import LazyLoad from 'react-lazyload';
 import img_01 from '../img/space_img01.jpg';
 
-const ImageOne = () => (
-  // <LazyLoad height={200} offset={100}>
+const ImageOne = () => {
+  useEffect(() => {
+    const handleScroll = throttle (() => {
+
+    }, 200);
+
+    window.addEventListener('scroll', handleScroll);
+
+    return() => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    // <LazyLoad height={200} offset={100}>
     <Parallax
       className='image'
       blur={0}
@@ -17,5 +32,6 @@ const ImageOne = () => (
     </Parallax>
   // </LazyLoad>
 );
+  };
 
 export default ImageOne;
